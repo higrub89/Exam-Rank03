3039cc83c05ed18c00ae1695a0a5b7bc8f2118bd
higrub89 <higuitaruben@hotmail.com>
2025-09-26 13:09:54 +0200

    Filter corregido

--- lvl1/filter/filter.c
+++ lvl1/filter/filter.c
@@ -1,45 +1,76 @@
-#include <stdio.h>
+#define _GNU_SOURCE
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <string.h>
 
-int main(int ac, char **av)
+void ft_filter(char *buffer, const char *target)
 {
-  char buffer[1024];
-  ssize_t len;
-  ssize_t i;
-  int j;
+    int i = 0;
+    int t_len = strlen(target);
+    int j;
 
-  if (ac != 2 || av[1][0] == '\0')
-    return (1);
-  while ((len = read(0, buffer, 1024)) > 0)
-  {
-    if (len < 0)
+    while (buffer[i])
     {
-      perror("Error");
-      return (1);
-    }
-    buffer[len] = '\0';
-    i = 0;
-    while (i < len)
-    {
-      j = 0;
-      while (av[1][j] != '\0' && buffer[i + j] == av[1][j])
-        j++;
-      if (av[1][j] == '\0')
-      {
-        while (j > 0)
+        j = 0;
+        while (target[j] && buffer[i + j] && buffer[i + j] == target[j])
+            j++;
+        if (j == t_len)
         {
-          printf("*");
-          j--;
+            int k = 0;
+            while (k < t_len)
+            {
+                write(1, "*", 1);
+                k++;
+            }
+            i = i + t_len;
         }
-        i += strlen(av[1]);
-      }
-      else
-      {
-        printf("%c", buffer[i]);
-        i++;
-      }
+        else
+        {
+            write(1, &buffer[i], 1);
+            i++;
+        }
     }
-  }
-  return (0);
+}
+
+int main(int argc, char **argv)
+{
+    char temp[BUFFER_SIZE];
+    char *res = NULL;
+    char *buffer;
+    int total_read = 0;
+    ssize_t bytes;
+
+    if (argc != 2 || argv[1][0] == '\0')
+        return 1;
+    while ((bytes = read(0, temp, BUFFER_SIZE)) > 0)
+    {
+        buffer = realloc(res, (total_read + bytes + 1));
+        if (!buffer)
+        {
+            free(res);
+            perror("realloc");
+            return (1);
+        }
+        res = buffer;
+        memmove((res + total_read), temp, bytes);
+        total_read = total_read + bytes;
+        res[total_read] = '\0';
+    }
+
+    if (bytes < 0)
+    {
+        perror("read");
+        free(res);
+        return (1);
+    }
+    if (!res)
+        return 0;
+    ft_filter(res, argv[1]);
+    free(res);
+    return (0);
 }
