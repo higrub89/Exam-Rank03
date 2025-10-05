269d1333f24cdf96f4511349ab039861f934bfd4
higrub89 <higuitaruben@hotmail.com>
2025-10-05 12:35:47 +0200

    Ejercicios permutation

--- lvl2/permutation/permutations2.c
+++ lvl2/permutation/permutations2.c
@@ -1,53 +1,57 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-void sorted(char *str){
-	char tmp;
-	for(int i = 0; str[i]; i++){
-		for(int j = i+1; str[j]; j++){
-			if(str[i] > str[j]){
-				tmp = str[i];
-				str[i] = str[j];
-				str[j] = tmp;
+void sorted(char *s)
+{
+	char tem;
+	for (int i = 0; s[i]; i++)
+	{
+		for(int j = i + 1; s[j]; j++)
+		{
+			if (s[i] > s[j])
+			{
+				tem = s[i];
+				s[i] = s[j];
+				s[j] = tem;
 			}
 		}
 	}
 }
 
-void perm(char *str , char *result , int *used, int dep, int len)
+void permutation(char *s, char *result, int *used, int dep, int len)
 {
 	if (dep == len)
 	{
-		for(int i = 0; i < len; i++)
+		for (int i = 0; i < len; i++)
 		{
 			write(1, &result[i], 1);
 		}
-		write(1,"\n",1);
-		return;
+		write(1, "\n", 1);
 	}
 	for (int i = 0; i < len; i++)
 	{
-		if (used[i]) 
-			continue;
+		if (used[i])
+			continue ;
 		used[i] = 1;
-		result[dep] = str[i];
-		perm(str, result , used , dep + 1 , len);
-		used[i] = 0;		
+		result[dep] = s[i];
+		permutation(s, result, used, dep + 1, len);
+		used[i] = 0;
 	}
 }
 
-int main(int argc, char **argv) {
-	if (argc != 2)
-		return 1;
-
+int main(int ac, char **av)
+{
+	if (ac != 2)
+		return (1);
 	int i = 0;
-	while (argv[1][i])
+	while (av[1][i])
 		i++;
 	int len = i;
 	char *result = malloc(len + 1);
 	int *used = calloc(len, sizeof(int));
-	sorted(argv[1]);
-	perm(argv[1], result, used, 0, len);	
+	sorted(av[1]);
+	permutation(av[1], result, used, 0, len);
 	free(result);
 	free(used);
+	return (0);
 }
