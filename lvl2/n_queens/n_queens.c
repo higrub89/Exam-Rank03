51f13dd081aa09e8663a0e400442d4feca517a1e
Ruben Higuita <higuitaruben@hotmail.com>
2025-10-21 07:30:36 +0000

    Exam 03 correct

--- lvl2/n_queens/n_queens.c
+++ lvl2/n_queens/n_queens.c
@@ -1,53 +1,53 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int is_safe(int *positions, int current_col, int current_row)
+int is_safe(int *position, int current_col, int current_row)
 {
 	for (int prev_col = 0; prev_col < current_col; prev_col++)
 	{
-		int prev_row = positions[prev_col];
+		int prev_row = position[prev_col];
 		if (prev_row == current_row ||
-			prev_row - prev_col == current_row - current_col ||
+			prev_row - prev_col == current_row - current_col||
 			prev_row + prev_col == current_row + current_col)
 			return 0;
 	}
 	return 1;
 }
 
-void solve(int *positions, int col, int n)
+void solve(int *position, int col, int n)
 {
 	if (col == n)
 	{
-	   for (int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++)
 		{
 			if (i > 0)
 				printf(" ");
-			printf("%d", positions[i]);
+			printf("%d", position[i]);
 		}
 		printf("\n");
-		return;
+		return ;
 	}
-	for(int row = 0; row < n; row++)
+	for (int row = 0; row < n; row++)
 	{
-		if(is_safe(positions, col, row))
+		if (is_safe(position, col, row))
 		{
-			positions[col] = row;
-			solve(positions, col + 1, n);
+			position[col] = row;
+			solve(position, col + 1, n);
 		}
 	}
 }
 
 int main(int ac, char **av)
 {
-	if(ac == 2 && av[1][0] != '-')
+	if (ac == 2 && av[1][0] != '-')
 	{
 		int n = atoi(av[1]);
-		int *positions = malloc(sizeof(int) * n);
-		if (!positions)
+		int *position = malloc(sizeof(int) * n);
+		if (!position)
 			return 1;
-		solve(positions, 0, n);
-		free(positions);
+		solve(position, 0, n);
+		free(position);
+		return 0;
 	}
-	return 0;
+	return 1;
 }
--- lvl2/powerset/powerset.c
+++ lvl2/powerset/powerset.c
@@ -1,105 +1,65 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
-
-# define MAX_CANDIDATES 2
-
-bool is_solution(int *a, int *nums, int n, int target)
+int check_int(char **av, int ac)
 {
-  int i = 0;
-  int res = 0;
-
-  while(i < n)
+  int j = 1;
+  while (j < ac)
   {
-    if (a[i])
-      res += nums[i];
-    i++;
+    int i = 0;
+    if (av[j][i] == '-' || av[j][i] == '+')
+      i++;
+    while (av[j][i])
+    {
+      if (av[j][i] < '0' || av[j][i] > '9')
+        return (0);
+      i++;
+    }
+    j++;
   }
-  if (res == target)
-    return (true);
-  return (false);
+  return (1);
 }
 
-void print_solution(int *a, int *nums, int n)
+void solve(int *nums, int size, int target, int *subset, int index, int sub_size, int curr_sum)
 {
-  int i;
-
-  i = 0;
-  while (i < n)
+  if (index == size)
   {
-    if (a[i])
-      printf("%d", nums[i]);
-    i++;
-  }
-  printf("\n");
-}
-
-void build_candidate(int *c, int *nc)
-{
-  c[0] = 1;
-  c[1] = 0;
-  *nc = 2;
-}
-
-void make_move(int *a, int k, int value)
-{
-  a[k] = value;
-}
-
-void unmake_move(int *a, int k, int value)
-{
-  (void)a;
-  (void)k;
-  (void)value;
-}
-
-void powerset(int *a, int *nums, int k, int n, int target)
-{
-  int candidates[2];
-  int nc;
-  int i;
-
-  if (k == n)
-  {
-    if (is_solution(a, nums, n, target))
-      print_solution(a, nums, n);
+    if (curr_sum == target)
+    {
+      for (int i = 0; i < sub_size; i++)
+      {
+        printf("%d", subset[i]);
+        if (i != sub_size - 1)
+          printf(" ");
+      }
+      printf("\n");
+    }
     return ;
   }
-  else
-  {
-    build_candidate(candidates, &nc);
-    i = -1;
-    while(++i < nc)
-    {
-      make_move(a, k, candidates[i]);
-      powerset(a, nums, k + 1, n, target);
-      unmake_move(a, k, candidates[i]);
-    }
-  }
+  solve(nums, size, target, subset, index + 1, sub_size, curr_sum);
+  subset[sub_size] = nums[index];
+  solve(nums, size, target, subset, index + 1, sub_size + 1, curr_sum + nums[index]);
 }
 
 int main(int ac, char **av)
 {
-  int *a;
-  int *nums;
-  int n;
-  int target;
-  int i;
-  int k;
-
-  if (ac == 1)
-    return (1);
-  n = ac -2;
-  target = atoi(av[1]);
-  a = calloc(n, sizeof(int));
-  if (!nums)
-    return (free(a), 1);
-  i = -1;
-  k = 0;
-  while (++i < n)
-    nums[i] = atoi(av[i + 2]);
-  powerset(a, nums, k, n, target);
-  return (free(a), free(nums), 0);
+  if (ac > 3 || check_int(av, ac))
+  {
+    int target = atoi(av[1]);
+    int *nums = malloc(sizeof(int) * (ac - 2));
+    if (nums == NULL)
+    {
+      free(nums);
+      return (1);
+    }
+    for (int i = 0; i < ac - 2; i++)
+    {
+      nums[i] = atoi(av[i + 2]);
+    }
+    int subset[(ac - 2)];
+    solve(nums, (ac -2), target, subset, 0, 0, 0);
+    free(nums);
+    return (0);
+  }
+  return (1);
 }
